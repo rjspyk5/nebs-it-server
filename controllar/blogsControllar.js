3fafafafc6a3a56bf56f0e8cab9937d68010887f
rjspyk5 <rjspyk5@gmail.com>
2025-05-21 11:34:21 +0600

    blogs rest api complete

--- controllar/blogsControllar.js
+++ controllar/blogsControllar.js
@@ -1,8 +1,9 @@
 const Blogs = require("../model/blogsModel");
+const database = require("../services/database");
 
 const blogsControllar = {
   allBlogs: async (req, res, next) => {
-      try {
+    try {
       const result = await Blogs.find();
       return res.send({
         message: "Blogs data retrive Successfully",
@@ -13,10 +14,68 @@ const blogsControllar = {
       next(error);
     }
   },
-  singleBlog: async (req, res, next) => {},
-  createBlog: async (req, res, next) => {},
-  editBlog: async (req, res, next) => {},
-  deleteblog: async (req, res, next) => {},
+  singleBlog: async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+      const result = await database.find(Blogs, { _id: id });
+      res.status(200).send({
+        success: true,
+        data: result,
+        message: `Data retrive successfully `,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  createBlog: async (req, res, next) => {
+    const data = req.body;
+    try {
+      const result = await database.create(Blogs, data);
+      res.status(200).send({
+        success: true,
+        message: "Blogs data retrive successfully",
+        blogId: result?._id,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  editBlog: async (req, res, next) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+      const result = await database.update(Blogs, { _id: id }, data);
+
+      res
+        .status(200)
+        .send({
+          success: true,
+          message: "Blog Update Successfully",
+          data: result.modifiedCount,
+        });
+    } catch (error) {
+      next(error);
+    }
+  },
+  deleteblog: async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+      const result = await Blogs.deleteOne({ _id: id });
+
+      res
+        .status(200)
+        .send({
+          success: true,
+          message: "Delete Successfully",
+          data: result.deletedCount,
+        });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
-module.exports=blogsControllar
+module.exports = blogsControllar;
