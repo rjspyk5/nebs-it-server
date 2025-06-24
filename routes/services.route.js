d9f59cd6a4148eb328072d938e4169970db4998f
rjspyk5 <rjspyk5@gmail.com>
2025-06-24 18:59:20 +0600

    make api for services meta data

--- routes/services.route.js
+++ routes/services.route.js
@@ -4,6 +4,7 @@ const ServicesControllar = require("../controllar/ServicesControllar");
 servicesRouter = express.Router();
 
 servicesRouter.get("/:href", ServicesControllar.getServicesPage);
+servicesRouter.get("/meta/:href", ServicesControllar.getServicesMeta);
 servicesRouter.get("/", ServicesControllar.getServicesPage);
 servicesRouter.post("/", ServicesControllar.createService);
 servicesRouter.put("/:id", ServicesControllar.updateService);
